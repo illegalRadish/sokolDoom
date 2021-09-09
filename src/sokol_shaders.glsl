4f1cd101f7b08da42b9ad81e9aecf419dbedf2c1
Andre Weissflog <floooh@gmail.com>
2021-09-09 21:54:55 +0200

    fix display flip on GL backend

--- src/sokol_shaders.glsl
+++ src/sokol_shaders.glsl
@@ -1,5 +1,4 @@
-@vs vs
-
+@vs offscreen_vs
 layout(location=0) in vec2 in_pos;
 out vec2 uv;
 
@@ -23,6 +22,17 @@ void main() {
 }
 @end
 
+@vs display_vs
+@glsl_options flip_vert_y
+layout(location=0) in vec2 in_pos;
+out vec2 uv;
+
+void main() {
+    gl_Position = vec4(in_pos * 2.0 - 1.0, 0.5, 1.0);
+    uv = vec2(in_pos.x, 1.0 - in_pos.y);
+}
+@end
+
 // pixel shader to perform upscale-rendering to display framebuffer
 @fs display_fs
 
@@ -35,5 +45,5 @@ void main() {
 }
 @end
 
-@program offscreen vs offscreen_fs
-@program display vs display_fs
+@program offscreen offscreen_vs offscreen_fs
+@program display display_vs display_fs
